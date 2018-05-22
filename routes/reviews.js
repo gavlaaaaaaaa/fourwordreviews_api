1ccb231d7632a133137267106a528086c114b04b
gavlaaaaaaaa <gavlaaaaa5@gmail.com>
2018-05-22 21:38:42 +0100

    added ability to fetch review by ID

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all reviews. */
 router.get('/', function(req, res, next) {
 	res.locals.connection.query('SELECT * from review', function (error, results, fields) {
 		if (error) throw error;
 		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
 	});
 });
+
+/* GET review by ID */
+router.get('/:id', function(req, res, next) {
+	res.locals.connection.query('SELECT * from review where id = ?', [req.params.id], function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+
 
 router.post('/', function(req,res){
 	res.locals.connection.query('INSERT INTO review (product_id, product_name, word1, word2, word3, word4, rating, image_url) VALUES (?,?,?,?,?,?,?,?) ', [ req.body.product_id, req.body.product_name, req.body.word1, req.body.word2, req.body.word3, req.body.word4, req.body.rating, req.body.image_url ] , function (error,results,fields){
