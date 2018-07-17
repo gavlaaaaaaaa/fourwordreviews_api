7468a91475395b7a62fdaeb23cfa5a3445cb4859
gavlaaaaaaaa <gavlaaaaa5@gmail.com>
2018-07-17 22:07:48 +0100

    added ability to get suggested users to follow

--- routes/following.js
+++ routes/following.js
@@ -17,6 +17,15 @@ router.get('/:user_id1/follow/:user_id2', function(req, res, next) {
 	});
 });
 
+/* GET suggested */
+router.get('/suggested/:user_id', function(req, res, next) {
+	res.locals.connection.query('select user.* from user left join follow on user.id = follow.follows_user_id where user.id != ? and (follow.user_id != ? or follow.user_id is null)', [req.params.user_id, req.params.user_id], function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+
+
 /* POST Add follow for user */
 router.post('/', function(req,res){
 	res.locals.connection.query('INSERT INTO follow (user_id, follows_user_id) VALUES (?,?) ', [ req.body.user_id, req.body.follows_user_id ] , function (error,results,fields){
@@ -27,7 +36,7 @@ router.post('/', function(req,res){
 
 
 /* DELETE follow */
-router.delete('/:user_id/unfollow/:follows_user_id', function(req,res){
+router.delete('/', function(req,res){
         res.locals.connection.query('DELETE FROM follow where user_id = ? and follows_user_id = ?  ', [ req.body.user_id, req.body.follows_user_id ] , function (error,results,fields){
         if (error) throw error;
         return res.send({ error: false, data:results, message: 'Unfollowed successfully.'});
